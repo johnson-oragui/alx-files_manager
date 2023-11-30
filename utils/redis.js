882113ea9b4a65ac93281ff611c4223c568ce46a
johnson-oragui <johnson.oragui@gmail.com>
2023-11-30 02:54:41 -0800

    eslinting

--- utils/redis.js
+++ utils/redis.js
@@ -2,37 +2,33 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor () {
+  constructor() {
+    this.client = redis.createClient();
 
-        this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log(err.message);
+    });
+  }
 
-        this.client.on('error', (err) => {
-          console.log(err.message);
-        });
-    }
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key);
+    return value;
+  }
 
-    }
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    await asyncSet(key, value, 'EX', duration);
+  }
 
-    async get(key) {
-      const asyncGet = promisify(this.client.get).bind(this.client);
-      const value = await asyncGet(key);
-      return value;
-    }
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key);
+  }
 
-    async set(key, value, duration) {
-      const asyncSet = promisify(this.client.set).bind(this.client);
-      await asyncSet(key, value, 'EX', duration);
-    }
-
-    async del(key) {
-      const asyncDel = promisify(this.client.del).bind(this.client);
-      await asyncDel(key);
-    }
-
-    async close() {
-      await this.client.quit();
-    }
-
+  async close() {
+    await this.client.quit();
+  }
 }
 
 const redisClient = new RedisClient();
