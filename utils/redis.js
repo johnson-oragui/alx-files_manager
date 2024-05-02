89850092f73c7081b6f2685198d5dcfc117a0bc2
johnson-oragui <johnson-oragui@gmail.com>
2024-05-02 22:00:17 +0100

    added host and port to connection

--- utils/redis.js
+++ utils/redis.js
@@ -3,7 +3,10 @@ import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
+    this.client = redis.createClient({
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 6379, // Default Redis port
+    });
     this.asyncGet = promisify(this.client.get).bind(this.client);
     this.asyncSetex = promisify(this.client.setex).bind(this.client);
     this.asyncDel = promisify(this.client.del).bind(this.client);
