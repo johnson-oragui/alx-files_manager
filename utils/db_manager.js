b3efffa6fa9217411e63be852f48d370d3d63d7e
johnson-oragui <johnson-oragui@gmail.com>
2024-05-02 22:01:09 +0100

    added console.log for debugging purposes

--- utils/db_manager.js
+++ utils/db_manager.js
@@ -4,58 +4,64 @@ import dbclient from './db';
 class DBCrud {
   static async findUser(user) {
     try {
-      console.log('findUser', user);
-      return await dbclient.usersCollection.findOne(user);
+      const foundUser = await dbclient.usersCollection.findOne(user);
+      console.log('from DBCrud.findUser', foundUser);
+      return foundUser;
     } catch (err) {
-      console.error(err);
+      console.error('Error in DBCrud.findUser method: ', err);
       return false;
     }
   }
 
   static async createNewUser(newUserData) {
     try {
-      return dbclient.usersCollection.insertOne(newUserData);
+      const newUser = await dbclient.usersCollection.insertOne(newUserData);
+      return newUser;
     } catch (err) {
-      console.error(err);
+      console.error('error in DBCrud.createNewUser method: ', err);
       return false;
     }
   }
 
   static async addNewFile(newFileData) {
     try {
-      return dbclient.filesCollection.insertOne(newFileData);
+      const newFile = await dbclient.filesCollection.insertOne(newFileData);
+      return newFile;
     } catch (err) {
-      console.error(err);
+      console.error('error in DBCrud.addNewFile method: ', err);
       return false;
     }
   }
 
   static async findFile(file) {
     try {
-      console.log('findfile', file);
-      return await dbclient.filesCollection.findOne(file);
+      const foundFile = await dbclient.filesCollection.findOne(file);
+      console.log('findfile', foundFile);
+      return foundFile;
     } catch (err) {
-      console.error(err);
+      console.error('error in DBCrud.findFile method: ', err);
       return false;
     }
   }
 
   static async fileUpdate(queryData, fileData) {
     try {
-      console.log('findfile', fileData);
-      return await dbclient.filesCollection.updateOne(queryData, fileData);
+      const updatedFiled = await dbclient.filesCollection.updateOne(queryData, fileData);
+      console.log('updatefile', updatedFiled);
+      return updatedFiled;
     } catch (err) {
-      console.error(err);
+      console.error('error in DBCrud.fileUpdate method: ', err);
       return false;
     }
   }
 
   static async filesAggregate(aggregate) {
     try {
-      console.log('findfile', aggregate);
-      return await dbclient.filesCollection.aggregate(aggregate);
+      const aggFile = await dbclient.filesCollection.aggregate(aggregate);
+      console.log('findfile', aggFile);
+      return aggFile;
     } catch (err) {
-      console.error(err);
+      console.error('error in DBCrud..filesAggregate method: ', err);
       return false;
     }
   }
